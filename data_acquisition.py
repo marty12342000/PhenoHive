73356547a3230302a79fb706dc40bbbf74e81950
Martin Lallemand <m.lallemand@pepps.be>
2023-03-12 16:01:16 +0100

    Developpement du fichier d'acquisition et d'analyse d'image

--- data_acquisition.py
+++ data_acquisition.py
@@ -3,36 +3,79 @@
 Script python qui récupère les images et les mesures de poids et les envoies à la base de données influxDB
 '''
 
-
-
 import time
 import datetime
-'''
 import RPi.GPIO as GPIO
 from picamera2 import Picamera2, Preview
+from data_acquisition import process_pipeline
+import configparser
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-LED = 16
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED,GPIO.LOW)
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import ASYNCHRONOUS
 
-cam = Picamera2()
-#Determine the date in the console with : sudo date -s "2022-12-05 21:25:00"
-
-def photo():
+def photo(path):
 	cam.start_preview(Preview.NULL)
 	cam.start()
+	time.sleep(8)
 	date = datetime.datetime.now()
-	cam.capture_file("/home/pi/Desktop/memoire/image/%s.jpg"  % date)
+    path_img = path + "/%s.jpg"  % date
+	cam.capture_file(path)
 	cam.stop_preview()
 	cam.stop()
-'''
-while True:
-	#GPIO.output(LED, GPIO.HIGH)
-	print("High")
-	#photo()
-	time.sleep(2)
-	#GPIO.output(LED,GPIO.LOW)
-	print("Low")
-	time.sleep(2)
+
+	return path_img
+
+
+
+
+def main():
+    # Parse Config.ini file
+	parser = configparser.ConfigParser()
+	parser.read(config.ini)
+
+	led = int(parser["Pins"]["led"])
+
+	token = str(parser["InfluxDB"]["token"])
+	org = str(parser["InfluxDB"]["org"])
+	bucket = str(parser["InfluxDB"]["bucket"])
+	url = str(parser["InfluxDB"]["url"])
+
+	path = str(parser["Path_to_save_img"]["absolute_path"])
+
+    # Initialization of the camera and GPIO's
+    GPIO.setmode(GPIO.BOARD)
+	GPIO.setwarnings(False)
+	LED = led
+	GPIO.setup(LED, GPIO.OUT)
+	GPIO.output(LED,GPIO.LOW)
+	cam = Picamera2()
+    
+	# Initialization of the InfluxDB client
+	client = InfluxDBClient(
+    	url=url,
+    	token=token,
+    	org=org
+	)
+    
+
+    while True:
+		# Take photo
+        GPIO.output(LED, GPIO.HIGH)
+		path_img = photo(path)
+		time.sleep(2)
+		GPIO.output(LED,GPIO.LOW)
+
+		# Get numerical value from the photo
+		growth_value = process_pipeline(path_img)
+	
+		# Send data to the DB
+		write_api = client.write_api(write_options=ASYNCHRONOUS)
+		p = Point("my_measurement").field("Growth", float(data)).field("Growth_station_one", int(growth_value) )
+        write_api.write(bucket=bucket, org=org, record=p)
+
+		time.sleep(2)
+        
+
+if __name__ == "__main__":
+    main()
+
--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,7 @@
 '''
 Script python qui process les images prisent par la caméra pour évaluer la croissance des plantes
 '''
+
+def process_pipeline(img_path):
+    value = 0
+    return value
