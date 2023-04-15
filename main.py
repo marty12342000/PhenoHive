060d7720c71c87ea9ba7c3619192fcbe938b7d08
Martin Lallemand <m.lallemand@pepps.be>
2023-04-15 18:55:32 +0200

    Ajout du HX711

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+from hx711 import HX711
 
 
 
@@ -26,7 +27,7 @@ def init():
 	parser = configparser.ConfigParser()
 	parser.read('config.ini')
 	
-	global LED, token, org, bucket, url, path, pot_limit, channel, kernel_size, fill_size, cam, client, disp, WIDTH, HEIGHT, but_cal_prev,but_start_stop
+	global LED, token, org, bucket, url, path, pot_limit, channel, kernel_size, fill_size, cam, client, disp, WIDTH, HEIGHT, but_left, but_right, hx711, tare, raw_weight
 	LED = int(parser["Pins"]["led"])
     
 	token = str(parser["InfluxDB"]["token"])
@@ -61,51 +62,45 @@ def init():
             SPI_PORT,
             SPI_DEVICE,
             max_speed_hz=SPEED_HZ))
-	
-	
+    #Hx711 
+    hx = HX711(dout_pin=5, pd_sck_pin=6)
+    tare = sum(hx.get_raw_data())/5
+    raw_weight = 0
+
 	# Camera and LED init
-	cam = Picamera2()
+    cam = Picamera2()
 	gpio.setwarnings(False)
 	gpio.setup(LED, gpio.OUT)
 	gpio.output(LED,gpio.HIGH)
 	
 	#Button init
-	but_cal_prev = 21
-	but_start_stop = 16
-	gpio.setup(but_cal_prev, gpio.IN, pull_up_down=gpio.PUD_UP)
-	gpio.setup(but_start_stop, gpio.IN, pull_up_down=gpio.PUD_UP)
+	but_left = 21
+	but_right = 16
+	gpio.setup(but_left, gpio.IN, pull_up_down=gpio.PUD_UP)
+	gpio.setup(but_right, gpio.IN, pull_up_down=gpio.PUD_UP)
 
 
-def photo(path, preview = False):
-    cam.start_preview(Preview.NULL)
-    cam.start()
-    time.sleep(8)
+def photo(path, preview = False, time_to_wait = 8):
+     cam.start_preview(Preview.NULL)
+     cam.start()
+     time.sleep(time_to_wait)
 
-    if preview == False:
-        date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            
-    else :
-        date = "img"
-            
-            
-    path_img = path + "/%s.jpg"  % date
-    cam.capture_file(path_img)
-    cam.stop_preview()
-    cam.stop()
+     if preview == False:
+         date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")      
+     else :
+         date = "img" 
+        
+     path_img = path + "/%s.jpg"  % date
+     cam.capture_file(path_img)
+     cam.stop_preview()
+     cam.stop()
 
-    return path_img
+     return path_img
 
 
 def show_image(disp, path_img, WIDTH, HEIGHT):
-    # Initialize display.
-    disp.clear()
-    disp.begin()
-    # Load an image.
     image = Image.open(path_img)
-    # Resize the image and rotate it so matches the display.
     image = image.rotate(0).resize((WIDTH, HEIGHT))
-    # Draw the image on the display hardware.
-    print('Drawing image')
     disp.display(image)
     
     
@@ -115,18 +110,20 @@ def send_to_db(client, bucket, point, field, value):
 	write_api.write(bucket=bucket, record=p)
 
 
+def show_logo():
+    logo = Image.open("/home/pi/Desktop/phenostation/assets/logo_phenohive.jpg")
+    logo = logo.rotate(0).resize((128, 70))
+    disp.display(logo)
 
 def start_measuring():
     
-    disp.clear()
-    # Initialize display.
-    disp.begin()
-
-    img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+    img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 255, 255))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 15)
-    draw.text((0, 60), "Collecting data...", font=font, fill=(255, 255, 255))
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
+    draw.text((0, 60), "Collecting data...", font=font, fill=(0, 0, 0))
     disp.display(img)
+    show_logo()
+
     while True:
         # Take photo
         gpio.output(LED, gpio.LOW)
@@ -143,55 +140,97 @@ def start_measuring():
 
 def plant_preview():
     while True:
-        path_img = photo("/home/pi/Desktop/phenostation/Assets", preview = True)
+        path_img = photo("/home/pi/Desktop/phenostation/assets", preview = True, time_to_wait=1)
         show_image(disp, path_img, WIDTH, HEIGHT)
-        time.sleep(2)
-        
-        if gpio.input(but_cal_prev) == False:
+        if gpio.input(but_right) == False:
             show_menu()
             break
         
 def show_menu():
-    # Initialize display.
-    disp.clear()
-    # Initialize display.
-    disp.begin()
-
-    img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+    # Initialize display.    
+    img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 255, 255))
     draw = ImageDraw.Draw(img)
-
-    #Title
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
-    draw.text((4, 20), "PhenoHive", font=font, fill=(255, 255, 255))
-
     #Menu
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 15)
-    draw.text((40, 60), "Menu", font=font, fill=(255, 255, 255))
-
+    draw.text((40, 80), "Menu", font=font, fill=(0, 0, 0))
     #Button
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
-    draw.text((0, 140), "<-- Preview     Start -->", font=font, fill=(255, 255, 255))
-
+    draw.text((0, 130), "<-- Config        Start -->", font=font, fill=(0, 0, 0))
     disp.display(img)
+    show_logo()
+
+
+def show_cal_prev_menu():
+    img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    #Menu
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 13)
+    draw.text((13, 80), "Configuration", font=font, fill=(0, 0, 0))
+    #Button
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
+    draw.text((0, 130), "<-- Calib           Prev -->", font=font, fill=(0, 0, 0))
+    disp.display(img)
+    show_logo()
+
+
+def show_cal_menu():
+    img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    #Menu
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
+    draw.text((5, 80), "Tare :" +  str(tare), font=font, fill=(0, 0, 0))
+    draw.text((5, 80), "Raw_value :" +  str(raw_weight), font=font, fill=(0, 0, 0))
+    #Button
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
+    draw.text((0, 130), "<-- Measure    Back -->", font=font, fill=(0, 0, 0))
+    disp.display(img)
+    show_logo()
+
+
+def get_weight():
+    raw_weight = sum(hx.get_raw_data())/5
+    return raw_weight
+    
+
+
+def weight_calibration():
+    while True:
+        show_cal_menu()
+        if gpio.input(but_left) == False:
+                raw_weight = get_weight()
+        
+        if gpio.input(but_right) == False:
+                show_menu()
+                break
     
     
 def main():
-    init()
-    show_image(disp, "/home/pi/Desktop/phenostation/Assets/logo_phenohive.png", WIDTH, HEIGHT)
-    time.sleep(3)
     show_menu()
     while True:
         #Choose Button
-        if gpio.input(but_cal_prev) == False:
-            plant_preview()
-            time.sleep(5)
+        if gpio.input(but_left) == False:
+            show_cal_prev_menu()
+            time.sleep(1)
+            while True:
+                if gpio.input(but_left) == False:
+                    weight_calibration()
+                    time.sleep(1)
+                    break
+
+                if gpio.input(but_right) == False:
+                    plant_preview()
+                    time.sleep(1)
+                    break
             
-        if gpio.input(but_start_stop) == False:
+        if gpio.input(but_right) == False:
             start_measuring()
-            time.sleep(5)
-        
+
     
         
 
 if __name__ == "__main__":
+    init()
+    disp.clear()
+    disp.begin()
+    show_image(disp, "/home/pi/Desktop/phenostation/assets/logo_elia.jpg", WIDTH, HEIGHT)
     main()
