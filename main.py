2cd314a093ad91107cdf1459ac9b271e4db9396a
Martin Lallemand <m.lallemand@pepps.be>
2023-04-17 15:23:59 +0200

    Ajout du fichier show_display.py

--- main.py
+++ main.py
@@ -24,78 +24,81 @@ from hx711 import HX711
 
 def init():
     # Parse Config.ini file
-	parser = configparser.ConfigParser()
-	parser.read('config.ini')
-	
-	global LED, token, org, bucket, url, path, pot_limit, channel, kernel_size, fill_size, cam, client, disp, WIDTH, HEIGHT, but_left, but_right, hx711, tare, raw_weight
-	LED = int(parser["Pins"]["led"])
-    
-	token = str(parser["InfluxDB"]["token"])
-	org = str(parser["InfluxDB"]["org"])
-	bucket = str(parser["InfluxDB"]["bucket"])
-	url = str(parser["InfluxDB"]["url"])
-	
+    parser = configparser.ConfigParser()
+    parser.read('config.ini')
 
-	path = str(parser["Path_to_save_img"]["absolute_path"])
+    global LED, token, org, bucket, url, path, pot_limit, channel, kernel_size, fill_size, cam, client, disp, WIDTH, HEIGHT, but_left, but_right, hx, tare, raw_weight
+    LED = int(parser["Pins"]["led"])
+        
+    token = str(parser["InfluxDB"]["token"])
+    org = str(parser["InfluxDB"]["org"])
+    bucket = str(parser["InfluxDB"]["bucket"])
+    url = str(parser["InfluxDB"]["url"])
+        
 
-	pot_limit = int(parser["image_arg"]["pot_limit"])
-	channel = str(parser["image_arg"]["channel"])
-	kernel_size = int(parser["image_arg"]["kernel_size"])
-	fill_size = int(parser["image_arg"]["fill_size"])
-	
-	# InfluxDB client initialization
-	client = InfluxDBClient(url=url, token=token, org=org)
-	
-	# Screen initialization
-	WIDTH = 128
-	HEIGHT = 160
-	SPEED_HZ = 4000000
-	DC = 24
-	RST = 25
-	SPI_PORT = 0
-	SPI_DEVICE = 0
-	
-	disp = TFT.ST7735(
+    path = str(parser["Path_to_save_img"]["absolute_path"])
+
+    pot_limit = int(parser["image_arg"]["pot_limit"])
+    channel = str(parser["image_arg"]["channel"])
+    kernel_size = int(parser["image_arg"]["kernel_size"])
+    fill_size = int(parser["image_arg"]["fill_size"])
+        
+    # InfluxDB client initialization
+    client = InfluxDBClient(url=url, token=token, org=org)
+
+    #Hx711 
+    hx = HX711(dout_pin=5, pd_sck_pin=6)
+    tare = sum(hx.get_raw_data())/5
+    raw_weight = 0
+        
+    # Screen initialization
+    WIDTH = 128
+    HEIGHT = 160
+    SPEED_HZ = 4000000
+    DC = 24
+    RST = 25
+    SPI_PORT = 0
+    SPI_DEVICE = 0
+        
+    disp = TFT.ST7735(
         DC,
         rst=RST,
         spi=SPI.SpiDev(
             SPI_PORT,
             SPI_DEVICE,
             max_speed_hz=SPEED_HZ))
-    #Hx711 
-    hx = HX711(dout_pin=5, pd_sck_pin=6)
-    tare = sum(hx.get_raw_data())/5
-    raw_weight = 0
-
-	# Camera and LED init
+        
+        
+    # Camera and LED init
     cam = Picamera2()
-	gpio.setwarnings(False)
-	gpio.setup(LED, gpio.OUT)
-	gpio.output(LED,gpio.HIGH)
-	
-	#Button init
-	but_left = 21
-	but_right = 16
-	gpio.setup(but_left, gpio.IN, pull_up_down=gpio.PUD_UP)
-	gpio.setup(but_right, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setwarnings(False)
+    gpio.setup(LED, gpio.OUT)
+    gpio.output(LED,gpio.HIGH)
+        
+    #Button init
+    but_left = 21
+    but_right = 16
+    gpio.setup(but_left, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(but_right, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+
+
 
 
 def photo(path, preview = False, time_to_wait = 8):
-     cam.start_preview(Preview.NULL)
-     cam.start()
-     time.sleep(time_to_wait)
-
-     if preview == False:
-         date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")      
-     else :
-         date = "img" 
+    cam.start_preview(Preview.NULL)
+    cam.start()
+    time.sleep(time_to_wait)
+    if preview == False:
+        date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")      
+    else :
+        date = "img" 
         
-     path_img = path + "/%s.jpg"  % date
-     cam.capture_file(path_img)
-     cam.stop_preview()
-     cam.stop()
-
-     return path_img
+    path_img = path + "/%s.jpg"  % date
+    cam.capture_file(path_img)
+    cam.stop_preview()
+    cam.stop()
+    return path_img
 
 
 def show_image(disp, path_img, WIDTH, HEIGHT):
@@ -105,25 +108,28 @@ def show_image(disp, path_img, WIDTH, HEIGHT):
     
     
 def send_to_db(client, bucket, point, field, value): 
-	write_api = client.write_api(write_options=SYNCHRONOUS)
-	p = Point(point).field(field, int(value))
-	write_api.write(bucket=bucket, record=p)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    p = Point(point).field(field, int(value))
+    write_api.write(bucket=bucket, record=p)
 
 
 def show_logo():
     logo = Image.open("/home/pi/Desktop/phenostation/assets/logo_phenohive.jpg")
     logo = logo.rotate(0).resize((128, 70))
-    disp.display(logo)
+    return logo
 
-def start_measuring():
-    
+def show_measuring_menu():
     img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 255, 255))
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
     draw.text((0, 60), "Collecting data...", font=font, fill=(0, 0, 0))
+    logo = show_logo()
+    img.paste(logo, (0, 0))
     disp.display(img)
-    show_logo()
 
+def start_measuring():
+    
+    show_measuring_menu()
     while True:
         # Take photo
         gpio.output(LED, gpio.LOW)
@@ -156,8 +162,9 @@ def show_menu():
     #Button
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
     draw.text((0, 130), "<-- Config        Start -->", font=font, fill=(0, 0, 0))
+    logo = show_logo()
+    img.paste(logo, (0, 0))
     disp.display(img)
-    show_logo()
 
 
 def show_cal_prev_menu():
@@ -169,8 +176,9 @@ def show_cal_prev_menu():
     #Button
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
     draw.text((0, 130), "<-- Calib           Prev -->", font=font, fill=(0, 0, 0))
+    logo = show_logo()
+    img.paste(logo, (0, 0))
     disp.display(img)
-    show_logo()
 
 
 def show_cal_menu():
@@ -178,13 +186,15 @@ def show_cal_menu():
     draw = ImageDraw.Draw(img)
     #Menu
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
-    draw.text((5, 80), "Tare :" +  str(tare), font=font, fill=(0, 0, 0))
-    draw.text((5, 80), "Raw_value :" +  str(raw_weight), font=font, fill=(0, 0, 0))
+    draw.text((0, 80), "Tare :" +  str(tare), font=font, fill=(0, 0, 0))
+    draw.text((0, 95), "Raw val :" +  str(raw_weight), font=font, fill=(0, 0, 0))
     #Button
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
     draw.text((0, 130), "<-- Measure    Back -->", font=font, fill=(0, 0, 0))
+    logo = show_logo()
+    img.paste(logo, (0, 0))
     disp.display(img)
-    show_logo()
+
 
 
 def get_weight():
@@ -197,6 +207,7 @@ def weight_calibration():
     while True:
         show_cal_menu()
         if gpio.input(but_left) == False:
+                global raw_weight
                 raw_weight = get_weight()
         
         if gpio.input(but_right) == False:
